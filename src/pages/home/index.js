37c807d89809dbcc423a9a452f2fd194e47eff43
Wildemar Barbosa <demaroto69@gmail.com>
2023-09-15 08:44:12 -0300

    Update index.js

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -14,11 +14,11 @@ const Index = () => {
                     <div className={`bg-${theme} p-5 rounded-lg m-3`}>
                         <h1 className={`display-4 text-${themeText}`}>Seja bem-vindo!</h1>
                         <p className={`lead text-${themeText}`}>Este site contém ferramentas como calculadora de investimento de Crypto e Fiis, como geradores de jogos para loterias.</p>
-                        <p className={`lead text-${themeText}`}>Não solicitamos nenhum dado pessoal, nem fazemos recomendações de investimentos. Todos geradores, calculadoras e outras ferramentas sempre serão gratuito neste portal.</p>
+                        <p className={`lead text-${themeText}`}>Não solicitamos nenhum dado pessoal, nem fazemos recomendações de investimentos. Todos geradores, calculadoras e outras ferramentas são auxiliadores, todas elas são gratuitas neste portal.</p>
                         <hr className="my-4" />
                         <p className={`text-${themeText}`}>Estou continuamente desenvolvendo calculadoras úteis referente a recursos monetários. Em breve, atualizaremos com mais recurso.</p>
                         
-                        <Link className="btn bg-dark text-white btn-lg">Obrigado!</Link>
+                        <Link className="btn bg-dark text-white btn-lg">Obrigado!   </Link>
                     </div>
                 </div>
             <FooterComponent />
