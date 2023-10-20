bd459c497d6d860d30fa4809aaa7ab87d30f3d17
Wildemar Barbosa <demaroto69@gmail.com>
2023-10-20 09:03:01 -0300

    add novidades

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -4,6 +4,7 @@ import LinkListComponent from '../../components/LinkList.Component';
 import FooterComponent from '../../components/Footer.Component';
 import { Link } from 'react-router-dom';
 import AnunciosGoogleComponent from '../../components/AnunciosGoogle.Component';
+import { Calculator, Dice5 } from 'react-bootstrap-icons';
 const Index = () => {
     const theme = useSelector((state) => state.theme.value)
     const themeText = theme === 'dark' ? 'light' : 'dark';
@@ -12,14 +13,49 @@ const Index = () => {
             <LinkListComponent />
             <main className={[theme, "container", "h-100", `bg-${theme}`].join(" ")}>
                 <div className='row'>
-                    <div className={`bg-${theme} p-5 rounded-lg m-3`}>
+                    <div className={`bg-${theme} p-5 rounded-lg mt-3`}>
                         <h1 className={`display-4 text-${themeText}`}>Seja bem-vindo!</h1>
                         <p className={`lead text-${themeText}`}>Este site contém ferramentas como calculadora de investimento de Crypto e Fiis, como geradores de jogos para loterias.</p>
-                        <p className={`lead text-${themeText}`}>Não solicitamos nenhum dado pessoal, nem fazemos recomendações de investimentos. Todos geradores, calculadoras e outras ferramentas são auxiliadores, todas elas são gratuitas neste portal.</p>
+                        <p className={`lead text-${themeText}`}>Não solicitamos nenhum dado pessoal, nem fazemos recomendações de investimentos. Todos geradores, calculadoras e outras ferramentas serão auxiliadores financeiros.</p>
                         <hr className="my-4" />
-                        <p className={`text-${themeText}`}>Estou continuamente desenvolvendo calculadoras úteis referente a recursos monetários. Em breve, atualizaremos com mais recurso.</p>
+                    </div>
+
+                    <div>
+                    <h2 className={`text-${themeText}`}>Novidades</h2>
+                    <div className='row justify-content-center'>
+                        <div className="card col-md-4 col-sm-12" >
+                            
+                            <div className="card-body">
+                                <h5 className="card-title"><Calculator /> Calculadora Fundo Imobiliário</h5>
+                                <p className="card-text">Está ferramenta auxilia o investidor no controle dos ativos imobiliário, com alguns detalhes sobre os rendimentos mensais e preço médio por ano.</p>
+                                <Link to={`/fiis`} className="btn btn-primary">Conhecer</Link>
+                                <button disabled className="ms-1 btn btn-secondary">Saiba Mais</button>
+                            </div>
+                        </div>
+
+                        <div className="card col-md-4 col-sm-12">
+                            
+                            <div className="card-body">
+                                <h5 className="card-title"><Dice5 /> Gerador Lotofácil</h5>
+                                <p className="card-text">Esta ferramenta é um gerador de jogos para Loteria Lotofácil. Esta possuí dados dos sorteios passados, sendo possível verificar pontos dos jogos gerados.</p>
+                                <Link to={`/lotofacil`}  className="btn btn-primary">Conhecer</Link>
+                                <button disabled className="ms-1 btn btn-secondary">Saiba Mais</button>
+                            </div>
+                        </div>
+
+                        <div className="card col-md-4 col-sm-12">
+                           
+                            <div className="card-body">
+                                <h5 className="card-title"><Calculator /> Calculadora Cryptomoeda</h5>
+                                <p className="card-text">Esta calculadora, tem uma cara de Wallet (Cateira) Crypto, mas é calculadora mesmo. Esta o auxiliará nos preços dos ativos e dados reais de cada ativo.</p>
+                                <Link to={`/token`} className="btn btn-primary">Conhecer</Link>
+                                <button disabled className="ms-1 btn btn-secondary">Saiba Mais</button>
+                            </div>
+                        </div>
+
                         
-                        <Link className="btn bg-dark text-white btn-lg">Obrigado!   </Link>
+                    </div>
+
                     </div>
                 </div>
                 <AnunciosGoogleComponent />
