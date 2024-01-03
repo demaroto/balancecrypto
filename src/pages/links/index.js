f50b3681b3ad11a2be42d3ccada495b0ee7cbde1
Wildemar Barbosa <demaroto69@gmail.com>
2024-01-03 13:42:03 -0300

    add more links

--- src/pages/links/index.js
+++ src/pages/links/index.js
@@ -4,7 +4,8 @@ import LinkListComponent from '../../components/LinkList.Component';
 import FooterComponent from '../../components/Footer.Component';
 import { Link } from 'react-router-dom';
 import AnunciosGoogleComponent from '../../components/AnunciosGoogle.Component';
-import { Calculator, Dice5, Robot } from 'react-bootstrap-icons';
+import { Calculator, Dice5, Instagram, Robot, Youtube } from 'react-bootstrap-icons';
+import './links.css';
 const Index = () => {
     const theme = useSelector((state) => state.theme.value)
     const themeText = theme === 'dark' ? 'light' : 'dark';
@@ -16,12 +17,26 @@ const Index = () => {
                     
                
                     <div className='row h-100 justify-content-center align-items-center'>
-                        <div className="card col-10" >
+                        <div className="card col-10 p-0" >
                             
                             <Link target='_blank' to={`https://www.mql5.com/en/market/product/109431?source=balancecrypto.com.br`} className="btn btn-primary">
                             <div>
                                 <h4 className='h1'><Robot /></h4>
-                                <h5 className="card-title">FMW Auto Trend - EA Experts</h5>
+                                <h5 className="card-title">FMW Auto Trend - MQL5</h5>
+                            </div>
+                            </Link>
+
+                            <Link target='_blank' to={`https://www.youtube.com/channel/UCeo2jrw9dmSUmgugM5jDQhg`} className="btn btn-danger mt-2">
+                            <div>
+                                <h4 className='h1'><Youtube /></h4>
+                                <h5 className="card-title">FMW Auto Trend - Youtube</h5>
+                            </div>
+                            </Link>
+
+                            <Link target='_blank' to={`https://www.instagram.com/balancecrypto2024/`} className="btn bg-pink mt-2">
+                            <div>
+                                <h4 className='h1'><Instagram /></h4>
+                                <h5 className="card-title">FMW Auto Trend - Instagram</h5>
                             </div>
                             </Link>
                         </div>
