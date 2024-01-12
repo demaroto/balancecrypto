e2422f77661bb327e57bf2b360a2c08eb4f6a8a1
Wildemar Barbosa <demaroto69@gmail.com>
2024-01-12 12:51:52 -0300

    Updates

---
+++ precache-manifest.dc97a38763ce5e91b4efa0dbc7924555.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "006f534673ab715377b9",
+    "url": "/balancecrypto.github.io/static/css/main.19b4ce61.chunk.css"
+  },
+  {
+    "revision": "006f534673ab715377b9",
+    "url": "/balancecrypto.github.io/static/js/main.006f5346.chunk.js"
+  },
+  {
+    "revision": "090dbcf4a522cc8e7c4d",
+    "url": "/balancecrypto.github.io/static/js/1.090dbcf4.chunk.js"
+  },
+  {
+    "revision": "1e927c4b28c51662f792",
+    "url": "/balancecrypto.github.io/static/js/2.1e927c4b.chunk.js"
+  },
+  {
+    "revision": "cf88ee7c27ad42e64111",
+    "url": "/balancecrypto.github.io/static/js/runtime~main.cf88ee7c.js"
+  },
+  {
+    "revision": "c3acb01333c942c5be56756ddd7aebde",
+    "url": "/balancecrypto.github.io/index.html"
+  }
+];
