889781017b20b1bdc906f0981c13c57f103f0cd9
Wildemar Barbosa <demaroto69@gmail.com>
2024-01-12 16:20:35 -0300

    Updates

---
+++ precache-manifest.ab12ba9c94869537efc27a3878cc9b6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "006f534673ab715377b9",
+    "url": "./static/css/main.19b4ce61.chunk.css"
+  },
+  {
+    "revision": "006f534673ab715377b9",
+    "url": "./static/js/main.006f5346.chunk.js"
+  },
+  {
+    "revision": "090dbcf4a522cc8e7c4d",
+    "url": "./static/js/1.090dbcf4.chunk.js"
+  },
+  {
+    "revision": "1e927c4b28c51662f792",
+    "url": "./static/js/2.1e927c4b.chunk.js"
+  },
+  {
+    "revision": "1edebda5aa3d134f1d8c",
+    "url": "./static/js/runtime~main.1edebda5.js"
+  },
+  {
+    "revision": "50802536a0c9274ac2782cd06710578f",
+    "url": "./index.html"
+  }
+];
